909f0d6ce1dcd0d50d38dd269208b10f9fc1f60c
Nic Bonetto <nick.bonetto@gmail.com>
2018-09-21 21:32:20 -0700

    routes: Added GET /spotify/artist endpoint.
    Signed-off-by: Nic Bonetto <nick.bonetto@gmail.com> #3

--- server/routes.js
+++ server/routes.js
@@ -1,6 +1,7 @@
 import Spotify from 'spotify-web-api-node';
 import checkSchema from 'express-validator';
 import * as validations from './validation';
+import * as spotifyHelper from '../lib/spotify';
 
 const spotify = new Spotify({
   clientId: process.env.SPOTIFY_ID,
@@ -23,5 +24,19 @@ export default app => {
   app.get('/spotify/autocomplete', checkSchema(validations.spotifyAutocomplete), async (req, res) => {
     const suggestions = await spotify.searchArtists(`artist:${req.query.term}*`);
     res.json(suggestions);
+  });
+
+  app.get('/spotify/artist', checkSchema(validations.spotifyArtist),async (req, res) => {
+    const artist = spotifyHelper.separateAristName(req.query.artist);
+    try {
+      const spotifyQuery = await spotify.searchArtists(artist);
+      const found = spotifyQuery.body.artists.items.filter(data => {
+        if (data.name === artist) return data;
+      });
+      res.json(found);
+    } catch (e) {
+      console.log(e);
+      res.sendStatus(500);
+    }
   });
 };
