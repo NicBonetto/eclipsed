c4c2776a7e745772f72416cb58839a0c34ea1b35
Nic Bonetto <nick.bonetto@gmail.com>
2018-09-26 19:54:48 -0700

    fixes: Moved Header styled components from layout to pages for searchPage.
    Signed-off-by: Nic Bonetto <nick.bonetto@gmail.com> #12

--- client/layouts/app.js
+++ client/layouts/app.js
@@ -11,34 +11,10 @@ const Wrapper = styled.div`
   );
 `;
 
-const HeaderContainer = styled.div`
-  display: flex;
-  flex-direction: row;
-  justify-content: center;
-  align-items: center;
-`;
-
-const Header = styled.div`
-  color: rgb(162, 167, 171);
-  font-family: 'Roboto', sans-serif;
-  font-weight: 300;
-  letter-spacing: 1;
-`;
-
 const App = ({ children }) => {
   return (
     <Wrapper>
       <Navbar/>
-      <HeaderContainer>
-        <Header>
-          <h3>Hype</h3>
-        </Header>
-      </HeaderContainer>
-      <HeaderContainer>
-        <Header>
-          <p>Discover artists through an infinite chain of suggestions</p>
-        </Header>
-      </HeaderContainer>
       { children }
     </Wrapper>
   );
--- client/pages/searchPage.js
+++ client/pages/searchPage.js
@@ -1,9 +1,34 @@
 import React from 'react';
+import styled from 'styled-components';
 import App from '../layouts/app';
+
+const HeaderContainer = styled.div`
+display: flex;
+flex-direction: row;
+justify-content: center;
+align-items: center;
+`;
+
+const Header = styled.div`
+color: rgb(162, 167, 171);
+font-family: 'Roboto', sans-serif;
+font-weight: 300;
+letter-spacing: 1;
+`;
 
 const SearchPage = () => {
   return (
     <App>
+      <HeaderContainer>
+        <Header>
+          <h3>Hype</h3>
+        </Header>
+      </HeaderContainer>
+      <HeaderContainer>
+        <Header>
+          <p>Discover artists through an infinite chain of suggestions</p>
+        </Header>
+      </HeaderContainer>
     </App>
   );
 };
