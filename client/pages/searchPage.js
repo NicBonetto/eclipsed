905f4669628fbaf2bf3635acd9208601be4c76a1
Nic Bonetto <nick.bonetto@gmail.com>
2018-10-06 21:29:23 -0700

    pages: Added searchPage.
    Signed-off-by: Nic Bonetto <nick.bonetto@gmail.com> #12

--- client/pages/searchPage.js
+++ client/pages/searchPage.js
@@ -1,19 +1,20 @@
 import React from 'react';
 import styled from 'styled-components';
 import App from '../layouts/app';
+import SearchBar from '../components/searchBar';
 
 const HeaderContainer = styled.div`
-display: flex;
-flex-direction: row;
-justify-content: center;
-align-items: center;
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
 `;
 
 const Header = styled.div`
-color: rgb(162, 167, 171);
-font-family: 'Roboto', sans-serif;
-font-weight: 300;
-letter-spacing: 1;
+  color: rgb(162, 167, 171);
+  font-family: 'Roboto', sans-serif;
+  font-weight: 300;
+  letter-spacing: 1;
 `;
 
 const SearchPage = () => {
@@ -29,6 +30,7 @@ const SearchPage = () => {
           <p>Discover artists through an infinite chain of suggestions</p>
         </Header>
       </HeaderContainer>
+      <SearchBar/>
     </App>
   );
 };
